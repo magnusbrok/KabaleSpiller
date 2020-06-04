292407a5d91f1de2cf49ce5b75da0cf0bd53cd98
Siff <siff.ravn@gmail.com>
2020-06-04 16:03:56 +0200

    Implememt client to send JSON

--- Client_Socket.py
+++ Client_Socket.py
@@ -1,5 +1,7 @@
 import socket
 import pickle
+import json
+from DTO.cardDTO import CardDTO, CardEncoder
 
 # Kilde: https://pythonprogramming.net/pickle-objects-sockets-tutorial-python-3/'
 # https://stackoverflow.com/questions/4185242/communication-between-python-client-and-java-server
@@ -10,15 +12,22 @@ PORT = 8080
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-sock.sendall(b'Hello\r\n')
-data = sock.recv(1024)
-print ("1)", data)
+card = CardDTO('C', 3)
+print(card)
 
-if ( data == b'olleH\r\n'):
-    sock.sendall(b'Bye\n')
-    data = sock.recv(1024)
-    print ("2)", data)
+data = json.dumps(card, cls=CardEncoder)
 
-    if (data == b'eyB\r\n'):
-        sock.close()
-        print ("Socket closed")
+sock.sendall(bytes(data, encoding='utf8'))
+
+# sock.sendall(b'Hello\r\n')
+# data = sock.recv(1024)
+# print ("1)", data)
+#
+# if ( data == b'olleH\r\n'):
+#     sock.sendall(b'Bye\n')
+#     data = sock.recv(1024)
+#     print ("2)", data)
+#
+#     if (data == b'eyB\r\n'):
+#         sock.close()
+#         print ("Socket closed")
