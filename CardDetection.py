ab57165a1db10a4f805f5445fa994204ca494e62
magnusbrok <magnus.brok@db-data.dk>
2020-06-10 17:06:05 +0200

    Update CardDetection.py

--- CardDetection.py
+++ CardDetection.py
@@ -9,32 +9,26 @@ import Cards
 def main():
 
     cap = cv2.VideoCapture(1)
-
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
     #cap = cv2.VideoCapture('http://192.168.1.135:4905/video')
-    while True:
-
-        ret, frame = cap.read()
-        cv2.imshow("Card",frame)
-        #img = cv2.imread(frame, 0)
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("p"):
-            image = frame
-            break
 
     while True:
         ret, frame = cap.read()
-        #frame = cv2.rotate(frame, cv2.ROTATE_180) # Only relevant for Magnus
-        cv2.imshow("tesss", frame)
-        #frame = imutils.resize(frame, Cards.feed_width, Cards.feed_hight)
-        cv2.imshow("test", frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("gray", gray)
 
-        dilate = Cards.preprocces_image(frame)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # float
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float
+
+        print_frame = frame[20:height, 650:width - 650]
+        save_frame = frame[20:height, 650:width - 650]
+
+        print_frame = cv2.resize(print_frame, (Cards.feed_width, Cards.feed_hight))
+        save_frame = cv2.resize(save_frame, (Cards.feed_width, Cards.feed_hight))
+
+        cv2.imshow("print frame", print_frame)
+
+        gray = cv2.cvtColor(print_frame, cv2.COLOR_BGR2GRAY)
+
+
+        dilate = Cards.preprocces_image(print_frame)
 
         contours, hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -42,7 +36,7 @@ def main():
 
         #pre_proc = Cards.preprocess_image(frame)
 
-        Cards.draw_board(frame)
+        Cards.draw_board(print_frame)
 
 
 
@@ -51,13 +45,13 @@ def main():
 
         for cnt in contours:
             area = cv2.contourArea(cnt)
-            print(cv2.contourArea(cnt))
+            #print(cv2.contourArea(cnt))
             if area >= 750:
                 temp_contours.append(cnt)
 
-        cv2.drawContours(frame, temp_contours, -1, (0, 255, 0), 3)
+        cv2.drawContours(print_frame, temp_contours, -1, (0, 255, 0), 3)
 
-        cv2.imshow('Contours', frame)
+        cv2.imshow('Contours', print_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
